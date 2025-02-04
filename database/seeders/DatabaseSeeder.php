2c9c634183839b135b5e675bc47eb8992a619a6c
Alexander Palacio <japalacio@sena.edu.co>
2025-02-04 09:30:50 -0500

    Creation of permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create permissions
+        $permissions = [
+            'super_admin',
+            'administrador',
+            'contratista',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // Create roles
+        $role = Role::create(['name' => 'Super_admin']);
+        $roleAdmin = Role::create(['name' => 'Admin']);
+        $roleContratista = Role::create(['name' => 'Contratista']);
+
+        // Assign permissions to roles
+        $role->syncPermissions(Permission::all());
+        $roleAdmin->givePermissionTo('administrador');
+        $roleContratista->givePermissionTo('contratista');
+
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+         // Assign role to user
+         $user->assignRole($role);
     }
 }
