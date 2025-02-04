a4b26186c2f165e957d054ff45bd00dd9c1b44f2
Diana <d.velezposso@gmail.com>
2025-02-04 11:36:01 -0500

    actualizacion permisos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create permissions
+        $permissions = [
+            'super_admin',
+            'administrador',
+            'contratista',
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // Create roles
+        $role = Role::create(['name' => 'Super_admin']);
+        $roleAdmin = Role::create(['name' => 'Admin']);
+        $roleContratista = Role::create(['name' => 'Contratista']);
+
+        // Assign permissions to roles
+        $role->syncPermissions(Permission::all());
+        $roleAdmin->givePermissionTo('administrador');
+        $roleContratista->givePermissionTo('contratista');
+
+        // User::factory(10)->create();
+
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+         // Assign role to user
+         $user->assignRole($role);
     }
 }
